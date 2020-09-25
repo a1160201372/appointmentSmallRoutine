8523a016d58671ad6fa872b55c5708b2e5d8e59f
PSZ <1160201372@qq.com>
2020-09-25 13:28:49 +0800

    1.1.1
    临时保存，完成了个人资料的设置

--- miniprogram/pages/main/setMe/index.js
+++ miniprogram/pages/main/setMe/index.js
@@ -16,12 +16,36 @@ Page({
     date: '1990-01',
     height_array:[],
     height_num:10,
-    basicInfo:["性别","年龄","学历","居住地","身高","职业"],
+    weight_array:[],
+    weight_num:10,
+
+    basicInfo:["性别","年龄","身高","体重","民族","学历","婚姻情况","居住地","职业"],
+
+    singlePick:["身高","体重","民族","学历","婚姻状况","月薪","购房情况","购车情况"],
+    singlePickRange:[
+      [],//身高
+      [],//体重
+      ["未选择","汉族","蒙古族","回族","藏族","维吾尔族","苗族","彝族","壮族","布依族","朝鲜族","满族","侗族","瑶族","白族","土家族",  
+      "哈尼族","哈萨克族","傣族","黎族","傈僳族","佤族","畲族","高山族","拉祜族","水族","东乡族","纳西族","景颇族","柯尔克孜族",  
+      "土族","达斡尔族","仫佬族","羌族","布朗族","撒拉族","毛南族","仡佬族","锡伯族","阿昌族","普米族","塔吉克族","怒族", "乌孜别克族",  
+     "俄罗斯族","鄂温克族","德昂族","保安族","裕固族","京族","塔塔尔族","独龙族","鄂伦春族","赫哲族","门巴族","珞巴族","基诺族"],//民族
+     ["初中","高中","大专","本科","硕士","博士"],//学历
+     ["未婚","离异","丧偶"],//婚姻状况
+     ["1千以下","1~2千","2~3千","3~4千","4~8千","8千~1万","1~2万","2~5万","五万以上"],//月薪
+     ["已购房{有贷款}","已购房{无贷款}","有能力购房","无房","无房希望对方解决","无房希望双方解决","与父母同住","独自租房","与人合租","住亲朋家","住单位房"],//购房情况
+     ["无车","已购车-经济型","已购车-中档型","已购车-豪华型","单位用车","需要时购置"],
+
+    ],
+
+    singlePickChangeFunction:["bindPickerChangeHeight"],
+    singlePickFlag:[3,9,0,0,0,0,0,0],//单项选择器的标志
+
+
     educationNum:0,
     bindKeyProfession:'',
     imgArray:[],
     imgNum:0,
-    userImg:["cloud://ceshi-fdybb.6365-ceshi-fdybb-1302833646/my-image.jpg",""],
+    //userImg:["cloud://ceshi-fdybb.6365-ceshi-fdybb-1302833646/my-image.jpg",""],
     tempFilePaths1:[],
     stampImg:"",
     openID:"",
@@ -123,9 +147,26 @@ Page({
       date:e.detail.value
     })
   },
+
+  singlePickChangeFunction:function(e){
+
+  },
+
   bindPickerChangeHeight:function(e){
+    console.log(e.currentTarget.dataset.index)
+
+    var num=e.currentTarget.dataset.index
+    var tmp=this.data.singlePickFlag
+
+    tmp[num]=e.detail.value
+
     this.setData({
-      height_num:e.detail.value
+      singlePickFlag:tmp
+    })
+  },
+  bindPickerChangeWeight:function(e){
+    this.setData({
+      weight_num:e.detail.value
     })
   },
   bindKeyProfession:function(e){
@@ -149,13 +190,18 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var tmp=[]
+    var tmp=this.data.singlePickRange;
+
     for(var i=150;i<210;i++)
     {
-      tmp=   tmp.concat(i+"cm");
+      tmp[0]= tmp[0].concat(i+"cm");
+    }
+    for(var i=40;i<150;i++)
+    {
+      tmp[1]= tmp[1].concat(i+"Kg");
     }
     this.setData({
-      height_array:tmp
+      singlePickRange:tmp
     })
   },
 
